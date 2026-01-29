a8a9642ad196311bdb9c5285406425a867ec7b39
gokul <jgokul0000@gmail.com>
2026-01-29 10:13:46 +0530

    finall db correction

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const dotenv=require("dotenv")
+const dotenv = require("dotenv")
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,52 +9,52 @@ dotenv.config();
 // ==========================
 // CONNECT TO MONGODBn   ok thanks for your information
 // ==========================
-mongoose.connect("mongodb+srv://jgokul0000_db_user:Hw0ifxRdP3WqC6ys@cluster0.msjhj9p.mongodb.net/?appName=Cluster0")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.error(err));
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
 // ==========================
 // MONGOOSE SCHEMA + MODEL
 
 // ==========================
 const PersonSchema = new mongoose.Schema({
-name: String,
-age: Number
+    name: String,
+    age: Number
 });
 const Person = mongoose.model("Person", PersonSchema);
 // ==========================
 // GET ALL PEOPLE
 // ==========================
 app.get("/", async (req, res) => {
-const people = await Person.find();
-res.json(people);
+    const people = await Person.find();
+    res.json(people);
 });
 // ==========================
 // ADD NEW PERSON
 // ==========================
 app.post("/", async (req, res) => {
-const newPerson = await Person.create(req.body);
-res.json(newPerson);
+    const newPerson = await Person.create(req.body);
+    res.json(newPerson);
 });
 // ==========================
 // UPDATE PERSON
 // ==========================
 app.put("/:id", async (req, res) => {
-const updated = await Person.findByIdAndUpdate(
-req.params.id,
-req.body,
-{ new: true }
-);
-res.json(updated);
+    const updated = await Person.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    res.json(updated);
 });
 // ==========================
 // DELETE PERSON
 // ==========================
 app.delete("/:id", async (req, res) => {
-await Person.findByIdAndDelete(req.params.id);
-res.json({ message: "Person Deleted" });
+    await Person.findByIdAndDelete(req.params.id);
+    res.json({ message: "Person Deleted" });
 });
 // ==========================
-const PORT =process.env.PORT ||4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-console.log("Server running on http://localhost:4000");
+    console.log("Server running on http://localhost:4000");
 });
